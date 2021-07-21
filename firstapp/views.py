0fba2c4775e39ce609970d3e5cbe6659b35471d3
Vishwanath Pethri Kamath <vishwanathkamath@gmail.com>
2021-07-21 09:29:31 +0000

    First commit

---
+++ firstapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from datetime import date
+from django.http import HttpResponse
+
+def index(request):
+    # Create a simple html page as a string
+    template = "<html>" \
+                "This is your first view" \
+               "</html>"
+    # Return the template as content argument in HTTP response
+    return HttpResponse(content=template)
+
+
+def get_date(request):
+    today = date.today()
+    template = "<html>" \
+                "Today's date is {}" \
+               "</html>".format(today)
+    return HttpResponse(content=template)
